d3965e5f836e558a82eda9f97dd4aa1495a8b015
Shivanshu <75486759+saurabh3103-stack@users.noreply.github.com>
2025-01-02 14:37:21 +0530

    Delete backend directory

--- backend/generateHash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-async function generateHash() {
-  const salt = await bcrypt.genSalt(10);  // Generate salt with 10 rounds
-  const hash = await bcrypt.hash('password123', salt);  // Replace 'password123' with your password
-  console.log(hash);  // Print the hashed password
-}
-
-generateHash();
